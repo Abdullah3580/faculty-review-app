04b7cca54855a9f4282440a72b06758ccccf55b5
Abdullah3580 <mdabdullahalsiam358@gmail.com>
2025-11-26 18:23:22 +0600

    starting from begining

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,5 +1,6 @@
 // src/app/verify-email/page.tsx
 // এটা server component
+import VerifyEmailClient from "./VerifyEmailClient";
 export default function VerifyEmailPage() {
   return <VerifyEmailClient />;
 }
